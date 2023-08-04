0cff952172fddc1cf802c93b7fa503edefe7d8ca
SSEMUGABI JAMES <95012336+ssemugabijames@users.noreply.github.com>
2023-08-04 18:29:08 +0300

    v4

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+
+export { default as Navbar } from "./Navbar.jsx";
+export { default as Billing } from "./Billing.jsx";
+export { default as CardDeal } from "./CardDeal.jsx";
+export { default as Business } from "./Business.jsx";
+export { default as Clients } from "./Clients.jsx";
+export { default as CTA } from "./CTA.jsx";
+export { default as Stats } from "./Stats.jsx";
+export { default as Footer } from "./Footer.jsx";
+export { default as Testimonials } from "./Testimonials.jsx";
+export { default as Hero } from "./Hero.jsx";
